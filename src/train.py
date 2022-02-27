ae1d0774763371b6b7450429a38d5b24bc847fd9
Martin Boehm <martinboehm@mailbox.org>
2022-02-27 01:56:40 +0100

    Add working pipeline to train model

--- src/train.py
+++ src/train.py
@@ -1,64 +1,37 @@
 import numpy as np
 import pandas as pd
 from sklearn import model_selection
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, Normalizer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 # import pickle
 
 df = pd.read_csv("../data/auto-mpg.csv", sep=";")
 
-#df["gewicht2"] = np.square(df["gewicht"])
-#df["gewicht^3"] = np.power(df["gewicht"], 3)
-#df["ps^2"] = np.square(df["ps"])
-#df["ps^3"] = np.power(df["ps"], 3)
-# df["beschleunigung^2"] = np.square(df["beschleunigung"])
-# df["bschleunigung"]
+df["gewicht2"] = np.square(df["gewicht"])
+df["ps^2"] = np.square(df["ps"])
+df["ps^3"] = np.power(df["ps"], 3)
+df["beschleunigung^2"] = np.square(df["beschleunigung"])
 
 
-print(df.head())
+# print(df.head())
 
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(df.drop("mpg", axis=1),
-                                                                    df["mpg"], train_size=0.3)
+                                                                    df["mpg"], train_size=0.7,
+                                                                    random_state=1)
 
-normalisation = {}
-
-
-def normalise_col(data, min_, max_):
-    if min_ is None:
-        min_ = np.min(data)
-    if max_ is None:
-        max_ = np.max(data)
-    return data - min_ / (max_ - min_), (min_, max_)
-
-
-def normalise(features, target, target_str, with_min_max=False):
-    for col in features.columns:
-        if with_min_max:
-            min_ = normalisation[col][0]
-            max_ = normalisation[col][1]
-        else:
-            min_, max_ = None, None
-        features[col], normalisation[col] = normalise_col(
-            features[col], min_, max_)
-    target, normalisation[target_str] = normalise_col(target, min_, max_)
-
-
-# normalise(X_train, y_train, "mpg")
-X_train["gewicht^2"] = np.square(X_train["gewicht"])
-X_train["ps^2"] = np.square(X_train["ps"])
-X_test["gewicht^2"] = np.square(X_test["gewicht"])
-X_test["ps^2"] = np.square(X_test["ps"])
-
-print("Beginning fitting linear model...")
-model = Pipeline([
+print("Fitting linear model...")
+pipe = Pipeline([("scaler", MinMaxScaler(copy=False)),
                  ("linear_model", LinearRegression())])
-model.fit(X_train, y_train)
 
-# normalise(X_test, y_test, "mpg", True)
+pipe.fit(X_train, y_train)
+y_predict = pipe.predict(X_test)
 
-
-print(f"Model score: {model.score(X_test, y_test)}")
-coefficients = model.named_steps["linear_model"].coef_
+print(f"Model score: {pipe.score(X_test, y_test)}")
+print(f"MSE: {mean_squared_error(y_test, y_predict)}")
+coefficients = pipe.named_steps["linear_model"].coef_
 print(f"Model coefficients: {coefficients}")
+intercept = pipe.named_steps["linear_model"].intercept_
+print(f"Intercept: {intercept}")
