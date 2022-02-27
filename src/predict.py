18c7e419e2591e3b17c5377113d2c07a896a356f
Martin Boehm <martinboehm@mailbox.org>
2022-02-27 18:34:48 +0100

    Add initial draft for prediction script and improve train.py

---
+++ src/predict.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import pickle
+
+pipe = pickle.load(open("../data/models/final_model", "rb"))
+
+print("Enter values for features to make a prediction.")
+weight = int(input("Weight: "))
+acc = float(input("Acceleration: "))
+year = int(input("Construction year: "))
+# 3433, 12.0, 70
+feature_vector = pd.DataFrame(data={"gewicht": weight, "beschleunigung": acc, "baujahr": year},
+                              index=[0])
+predict = pipe.predict(feature_vector)
+print(f"Predicted miles per gallon: {predict}")
--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn import model_selection
 from sklearn.linear_model import Ridge
@@ -6,9 +7,18 @@ from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures
 from sklearn.metrics import mean_squared_error
 import pickle
 
+DROP_COLS = ("zylinder", "ps")
+
 df = pd.read_csv("../data/auto-mpg.csv", sep=";")
-df = df.drop("zylinder", axis=1)
-df = df.drop("ps", axis=1)
+
+
+def drop(columns, in_df):
+    for col in columns:
+        in_df = in_df.drop(col, axis=1)
+    return in_df
+
+
+df = drop(("zylinder", "ps"), df)
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(df.drop("mpg", axis=1),
                                                                     df["mpg"], train_size=0.7)
@@ -16,16 +26,17 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(df.drop("mpg
 print("Fitting linear model...")
 pipe = Pipeline([("scaler", MinMaxScaler(copy=False)),
                  ("polnomial", PolynomialFeatures(
-                     degree=(1, 3), include_bias=False)),
+                     degree=(1, 2), include_bias=False)),
                  ("linear_model", Ridge(alpha=0.1))])
 
 pipe.fit(X_train, y_train)
 y_predict_train = pipe.predict(X_train)
 y_predict_test = pipe.predict(X_test)
 
-
 print(f"Model score: {pipe.score(X_test, y_test)}")
-print(f"MSE on train set: {mean_squared_error(y_train, y_predict_train)}")
-print(f"MSE on test set: {mean_squared_error(y_test, y_predict_test)}")
+print(
+    f"RMSE on train set: {np.sqrt(mean_squared_error(y_train, y_predict_train))}")
+print(
+    f"RMSE on test set: {np.sqrt(mean_squared_error(y_test, y_predict_test))}")
 
 pickle.dump(pipe, open("../data/models/final_model", "wb"))
