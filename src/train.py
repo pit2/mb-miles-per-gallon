4262cd43803ed366a8e793f4a63dd7f3bc60ce30
Martin Boehm <martinboehm@mailbox.org>
2022-02-27 09:58:59 +0100

    Improve pipeline to include polynomial features and use Ridge regression

--- src/train.py
+++ src/train.py
@@ -1,23 +1,20 @@
-import numpy as np
 import pandas as pd
 from sklearn import model_selection
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures
 from sklearn.metrics import mean_squared_error
 import pickle
 
 df = pd.read_csv("../data/auto-mpg.csv", sep=";")
-
-df["gewicht2"] = np.square(df["gewicht"])
-df["beschleunigung^2"] = np.square(df["beschleunigung"])
 
 X_train, X_test, y_train, y_test = model_selection.train_test_split(df.drop("mpg", axis=1),
                                                                     df["mpg"], train_size=0.7)
 
 print("Fitting linear model...")
 pipe = Pipeline([("scaler", MinMaxScaler(copy=False)),
-                 ("linear_model", LinearRegression())])
+                 ("polnomial", PolynomialFeatures(degree=(1, 3), include_bias=False)),
+                 ("linear_model", Ridge(alpha=0.1))])
 
 pipe.fit(X_train, y_train)
 y_predict_train = pipe.predict(X_train)
